a8aa0b4ef85b548628056fb55be42a1c6587ee4c
Dominic Udegbunam <missdallen@yahoo.com>
2018-10-09 19:06:50 -0400

    Second Commit

---
+++ movieSearch/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movieSearch
+{
+    class Movie
+    {
+        public string
+        public string[]
+        
+
+        //public string Movie { get; set; }
+        //public string Actor { get; set; }
+        //public string Genre { get; set; }
+        //public string Director { get; set; }
+
+
+    }
+}
---
+++ movieSearch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movieSearch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! Looks like your looking for a movie. Do you want to search by movie, actor, direcotr or add a movie");
+            userInput = Console.ReadLine;
+
+            {
+                
+            }
+
+            // List<string> movie = new List<string>();
+        }
+    }
+}
